91b16e874db7f026d2f1db0964eab302401ced82
Wang Yu <wangyu_onepach@qq.com>
2019-01-17 13:32:53 +0800

    test baidu ai

--- src/main/java/baidu/ai/Test.java
+++ src/main/java/baidu/ai/Test.java
@@ -1,21 +1,20 @@
 package baidu.ai;
 
+import com.baidu.aip.imageclassify.AipImageClassify;
 import com.baidu.aip.ocr.AipOcr;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.util.HashMap;
 
 public class Test {
     public static void main(String[] args) {
-        String img = "C:\\Users\\Ste\\Desktop\\file\\clean\\34002.jpg";
-        AipOcr ocrClient = BaiduAi.getOcrClient();
+        String img = "C:\\Users\\Ste\\Desktop\\file\\王禹0117\\7940.jpg";
+        AipImageClassify imageClassify = BaiduAi.getImageClient();
         HashMap<String, String> options = new HashMap<>();
-        options.put("recognize_granularity", "small");
-        options.put("language_type", "ENG");
-        String s = ocrClient.general(img, options).toString();
-        System.out.println(s);
-
-        SingleHandler.startHandle(new File(img));
+        options.put("top_num", "3");
+        JSONObject jsonObject = imageClassify.carDetect(img, options);
+        System.out.println(jsonObject.toString());
 
     }
 }
